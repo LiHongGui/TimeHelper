cdaf886c670b9b20471d330232835728874b7c5b
MichaelLi <1045359337@qq.com>
2019-09-10 11:26:13 +0800

    创建版本 1.0.1.

--- TimeHelper/TimeHelper.h
+++ TimeHelper/TimeHelper.h
@@ -7,13 +7,58 @@
 //
 
 #import <Foundation/Foundation.h>
-
+/**
+ *  弹出日期类型
+ */
+typedef enum{
+    /**
+     *年月日时分
+     */
+    YMDMinS  = 0,
+    /**
+     *年月日时分
+     */
+    YMD,
+    /**
+     *年月
+     */
+    YM,
+    MD,//月日
+    HM,//时分
+    Y,//年
+    M,//月
+    DMinS,//日时分
+}YMDMS;
 @interface TimeHelper : NSObject
+/**
+ *当前月多少天
+ */
++(NSUInteger)numberOfDaysInMonthWithDate:(NSDate *)date;
 +(NSString *)currentZeroTime;
-+(NSString *)whichDayZeroTime:(NSInteger )yearTime withMonthTime:(NSInteger )monthTime withDayTime:(NSInteger )dayTime;
-+(NSTimeInterval )whichTime:(NSString *)time;
++(NSString *)WhichDayZeroTime:(NSInteger )yearTime withMonthTime:(NSInteger )monthTime withDayTime:(NSInteger )dayTime;
 +(NSString *)currentWeekZeroTime;
 +(NSString *)currentMonthFirstDayTime;
 + (int)compareOneDay:(NSDate *)oneDay withAnotherDay:(NSDate *)anotherDay;
-+ (NSDate *)getCurrentTime;
++ (NSString *)getCurrentTime;
++(NSInteger)getNowTimestamp;
+/**
+ *时间戳(毫秒转时间格式)
+ */
++(NSString *)backGroundWith:(NSInteger)timestamp withYMDMS:(YMDMS)yMinDMSStyle;
+/*当前时间毫秒*/
++(NSTimeInterval )whichTime:(NSString *)time;
+/**
+ *当前时间
+ */
++ (NSString *)getCurrentYMDMS:(YMDMS)yMinDMSStyle;
++ (NSString *)getCurrentWeekDay;
++ (NSInteger )getCurrentDayDate;
+
++ (NSString *)getWhichYMDDate:(YMDMS)yMinDMSStyle withDate:(NSDate *)date;
++ (NSInteger )getSelectWhichNumberForSelectDate:(NSDate *)selectDate;
++ (NSString *)getSelectWhichweekDayForSelectWhichData:(NSDate *)selectDate;
+/**
+ *字符串转时间
+ */
++(NSDate *)nsstringConversionNSDate:(NSString *)dateStr;
 @end
--- TimeHelper/TimeHelper.m
+++ TimeHelper/TimeHelper.m
@@ -9,8 +9,19 @@
 //
 
 #import "TimeHelper.h"
+@interface TimeHelper()
+@property (nonatomic,assign)YMDMS yMinDMSStyle;
 
+@end
 @implementation TimeHelper
+//#pragma mark-代理:当前哪年那月
+#pragma mark-代理:当前月多少天
++(NSUInteger)numberOfDaysInMonthWithDate:(NSDate *)date
+{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSRange range = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date];
+    return range.length;
+}
 /*当前0点0分*/
 +(NSString *)currentZeroTime
 {
@@ -23,7 +34,7 @@
     return currentZeroTimeStr;
 }
 /*某一天0点0分*/
-+(NSString *)whichDayZeroTime:(NSInteger )yearTime withMonthTime:(NSInteger )monthTime withDayTime:(NSInteger )dayTime
++(NSString *)WhichDayZeroTime:(NSInteger )yearTime withMonthTime:(NSInteger )monthTime withDayTime:(NSInteger )dayTime
 {
     NSString *dataTime = [NSString stringWithFormat:@"%ld-%ld-%ld",(long)yearTime,(long)monthTime,(long)dayTime];
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
@@ -33,17 +44,6 @@
     NSTimeInterval dayZeroTime = [beginDate timeIntervalSince1970]*1000;
     NSString *dayZeroTimeStr = [NSString stringWithFormat:@"%.0f",dayZeroTime];
     return dayZeroTimeStr;
-}
-/*当前时间毫秒*/
-+(NSTimeInterval )whichTime:(NSString *)time
-{
-//    NSString *dataTime = [NSString stringWithFormat:@"%ld-%ld-%ld",(long)yearTime,(long)monthTime,(long)dayTime];
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"YYYY/MM/dd"];
-//    NSString* beginStr = [NSString stringWithFormat:@"%@ 00:00:00",dataTime];
-    NSDate *beginDate=[formatter dateFromString:time];
-    NSTimeInterval dayZeroTime = [beginDate timeIntervalSince1970]*1000;
-    return dayZeroTime;
 }
 /*本周日0点0分*/
 +(NSString *)currentWeekZeroTime
@@ -78,7 +78,7 @@
     
     NSDateComponents *lastDayComp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:now];
     [lastDayComp setDay:day + lastDiff];
-    NSDate *lastDayOfWeek= [calendar dateFromComponents:lastDayComp];
+//    NSDate *lastDayOfWeek= [calendar dateFromComponents:lastDayComp];
     
     NSDateFormatter *formater = [[NSDateFormatter alloc] init];
     [formater setDateFormat:@"yyyy-MM-dd"];
@@ -92,6 +92,17 @@
     NSString *timeStr = [NSString stringWithFormat:@"%ld",firstStamp];
     //XLog(@"timeStr:%@",timeStr);
     return timeStr;
+}
+/*当前时间毫秒*/
++(NSTimeInterval )whichTime:(NSString *)time
+{
+    //    NSString *dataTime = [NSString stringWithFormat:@"%ld-%ld-%ld",(long)yearTime,(long)monthTime,(long)dayTime];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YYYY/MM/dd HH:mm:ss"];
+    //    NSString* beginStr = [NSString stringWithFormat:@"%@ 00:00:00",dataTime];
+    NSDate *beginDate=[formatter dateFromString:time];
+    NSTimeInterval dayZeroTime = [beginDate timeIntervalSince1970]*1000;
+    return dayZeroTime;
 }
 //当月第一天
 +(NSString *)currentMonthFirstDayTime
@@ -109,6 +120,115 @@
     NSDate *firstDayOfCurrentMonth = [calendar dateFromComponents:firstDayCurrentMonth];
     NSString *timeStr = [NSString stringWithFormat:@"%ld", (long)[firstDayOfCurrentMonth timeIntervalSince1970]*1000];
     return timeStr;
+}
++ (NSString *)getCurrentWeekDay
+{
+    NSDate *now = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitWeekday
+                                         fromDate:now];
+    // 得到星期几
+    // 1(星期天) 2(星期二) 3(星期三) 4(星期四) 5(星期五) 6(星期六) 7(星期天)
+//    NSArray *arabicNumeralsArray = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"0"];
+    NSArray *chineseNumeralsArray = @[@"日",@"一",@"二",@"三",@"四",@"五",@"六"];
+    NSInteger weekDay = [comp weekday];
+    
+    return chineseNumeralsArray[weekDay -1];
+}
++ (NSString *)getWhichYMDDate:(YMDMS)yMinDMSStyle withDate:(NSDate *)date
+{
+    NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
+    switch (yMinDMSStyle) {
+        case YMDMinS:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+        }
+            break;
+        case YMD:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd"];
+        }
+            break;
+        case YM:
+        {
+            [formatter setDateFormat:@"yyyy-MM"];
+        }
+            break;
+        case MD:
+        {
+            [formatter setDateFormat:@"MM-dd"];
+        }
+            break;
+        case HM:
+        {
+            [formatter setDateFormat:@" hh:mm"];
+        }
+            break;
+        case Y:
+        {
+            [formatter setDateFormat:@"yyyy"];
+        }
+            break;
+        case M:
+        {
+            [formatter setDateFormat:@"MM"];
+        }
+            break;
+            
+        default:
+        {
+            [formatter setDateFormat:@"dd hh:mm"];
+        }
+            break;
+    }
+    NSString *dateTime=[formatter stringFromDate:date];
+    NSDate *dateChange = [formatter dateFromString:dateTime];
+    NSString *dateString       = [formatter stringFromDate: dateChange];
+    return dateString;
+}
++ (NSString *)getSelectWhichweekDayForSelectWhichData:(NSDate *)selectDate;
+{
+//    NSDate *now = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitWeekday
+                                         fromDate:selectDate];
+    // 得到星期几
+    // 1(星期天) 2(星期二) 3(星期三) 4(星期四) 5(星期五) 6(星期六) 7(星期天)
+    //    NSArray *arabicNumeralsArray = @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"0"];
+    NSArray *chineseNumeralsArray = @[@"日",@"一",@"二",@"三",@"四",@"五",@"六"];
+    NSInteger weekDay = [comp weekday];
+    
+    return chineseNumeralsArray[weekDay -1];
+}
+/**
+ *时间转换NSDate
+ */
++(NSDate *)nsstringConversionNSDate:(NSString *)dateStr
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"YYYY-MM-dd"];
+    NSDate *datestr = [dateFormatter dateFromString:dateStr];
+    return datestr;
+}
++ (NSInteger )getCurrentDayDate
+{
+    NSDate *now = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitWeekday
+                                         fromDate:now];
+    // 得到几号
+    NSInteger day = [comp day];
+    return day;
+}
++ (NSInteger )getSelectWhichNumberForSelectDate:(NSDate *)selectDate
+{
+//    NSDate *now = [NSDate date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *comp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitWeekday
+                                         fromDate:selectDate];
+    // 得到几号
+    NSInteger day = [comp day];
+    return day;
 }
 
 - (void)getCurrentWeek
@@ -142,14 +262,14 @@
     
     NSDateComponents *lastDayComp = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:now];
     [lastDayComp setDay:day + lastDiff];
-    NSDate *lastDayOfWeek= [calendar dateFromComponents:lastDayComp];
+//    NSDate *lastDayOfWeek= [calendar dateFromComponents:lastDayComp];
     
     NSDateFormatter *formater = [[NSDateFormatter alloc] init];
     [formater setDateFormat:@"yyyy-MM-dd"];
     //XLog(@"一周开始 %@",[formater stringFromDate:firstDayOfWeek]);
-    NSString *sundayString = [NSString stringWithFormat:@"%@",[formater stringFromDate:firstDayOfWeek]];
-    NSDate *lastDate = [formater dateFromString:sundayString];
-    long firstStamp = [lastDate timeIntervalSince1970]*1000;
+//    NSString *sundayString = [NSString stringWithFormat:@"%@",[formater stringFromDate:firstDayOfWeek]];
+//    NSDate *lastDate = [formater dateFromString:sundayString];
+//    long firstStamp = [lastDate timeIntervalSince1970]*1000;
     //XLog(@"firstStamp:%ld",firstStamp);
     //XLog(@"当前 %@",[formater stringFromDate:now]);
     //XLog(@"一周结束 %@",[formater stringFromDate:lastDayOfWeek]);
@@ -194,11 +314,138 @@
     ////XLog(@"Both dates are the same");
     
 }
-+ (NSDate *)getCurrentTime{
++ (NSString *)getCurrentTime{
     NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
     [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+//    [formatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
     NSString *dateTime=[formatter stringFromDate:[NSDate date]];
     NSDate *date = [formatter dateFromString:dateTime];
-    return date;
+    NSString *dateString       = [formatter stringFromDate: date];
+    return dateString;
 }
+#pragma mark-:当前时间
++ (NSString *)getCurrentYMDMS:(YMDMS)yMinDMSStyle{
+    NSDateFormatter *formatter=[[NSDateFormatter alloc]init];
+    switch (yMinDMSStyle) {
+        case YMDMinS:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+        }
+            break;
+        case YMD:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd"];
+        }
+            break;
+        case YM:
+        {
+            [formatter setDateFormat:@"yyyy-MM"];
+        }
+            break;
+        case MD:
+        {
+            [formatter setDateFormat:@"MM-dd"];
+        }
+            break;
+        case HM:
+        {
+            [formatter setDateFormat:@" hh:mm"];
+        }
+            break;
+        case Y:
+        {
+            [formatter setDateFormat:@"yyyy"];
+        }
+            break;
+        case M:
+        {
+            [formatter setDateFormat:@"MM"];
+        }
+            break;
+            
+        default:
+        {
+            [formatter setDateFormat:@"dd hh:mm"];
+        }
+            break;
+    }
+    
+    //    [formatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    NSString *dateTime=[formatter stringFromDate:[NSDate date]];
+    NSDate *date = [formatter dateFromString:dateTime];
+    NSString *dateString       = [formatter stringFromDate: date];
+    return dateString;
+}
+/*当前时间戳*/
++(NSInteger)getNowTimestamp{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    [formatter setDateFormat:@"HH:mm:ss"]; // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    //设置时区,这个对于时间的处理有时很重要
+    NSTimeZone* timeZone = [NSTimeZone timeZoneWithName:@"Asia/Beijing"];
+    [formatter setTimeZone:timeZone];
+    NSDate *datenow = [NSDate date];//现在时间
+    NSLog(@"设备当前的时间:%@",[formatter stringFromDate:datenow]);
+    //时间转时间戳的方法:
+    NSInteger timeSp = [[NSNumber numberWithDouble:[datenow timeIntervalSince1970]] integerValue]*1000;
+    NSLog(@"设备当前的时间戳:%ld",(long)timeSp); //时间戳的值
+
+    return timeSp;
+    
+}
+#pragma mark-代理:时间戳(毫秒转时间格式)
++(NSString *)backGroundWith:(NSInteger)timestamp withYMDMS:(YMDMS)yMinDMSStyle
+{
+    // iOS 生成的时间戳是10位
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    switch (yMinDMSStyle) {
+        case YMDMinS:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+        }
+            break;
+        case YMD:
+        {
+            [formatter setDateFormat:@"yyyy-MM-dd"];
+        }
+            break;
+        case YM:
+        {
+            [formatter setDateFormat:@"yyyy-MM"];
+        }
+            break;
+        case MD:
+        {
+            [formatter setDateFormat:@"MM-dd"];
+        }
+            break;
+        case HM:
+        {
+            [formatter setDateFormat:@"hh:mm"];
+        }
+            break;
+        case Y:
+        {
+            [formatter setDateFormat:@"yyyy"];
+        }
+            break;
+        case M:
+        {
+            [formatter setDateFormat:@"MM"];
+        }
+            break;
+
+        default:
+        {
+            [formatter setDateFormat:@"dd hh:mm"];
+        }
+            break;
+    }
+    NSTimeInterval interval    = timestamp / 1000.0;
+    NSDate *date               = [NSDate dateWithTimeIntervalSince1970:interval];
+    NSString *dateString       = [formatter stringFromDate: date];
+    return dateString;
+}
+
 @end
